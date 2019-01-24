ee68c462478c34fb3fe45ca08fbc037e8b7d50d7
Darun Seethammagari <dseet001@bolt.cs.ucr.edu>
2019-01-23 23:31:43 -0800

    phong shading runs - no passes though

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -10,34 +10,35 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     //TODO; determine the color
-    color = color_ambient * ambient_intensity;
+    color = color_ambient * world.ambient_intensity;
     Ray lightRay;
 
     for(unsigned i = 0; i < world.lights.size(); i++) {
     	//Compute light direction
-    	lightRay.direction = (world.lights[i].position - intersection_point).normalized(); //Points from object to light
-    	double lightDist = lightRay.direction.magnitude();
-    	lightRay.endpoint = world.lights[i].position;
+    	lightRay.direction = (world.lights[i]->position - intersection_point).normalized(); //Points from object to light
+    	double lightDist = lightRay.direction.magnitude_squared();
+    	lightRay.endpoint = world.lights[i]->position;
 
     	//Check if object is in shadow
     	if(world.enable_shadows) {
-    		ray ShadowRay;
+    		Ray shadowRay;
     		shadowRay.direction = lightRay.direction;
     		shadowRay.endpoint = intersection_point;
-    		Hit shadowCheck = Closest_Intersection(lightRay);
-    		if ((shadowCheck.t * lightRay).magnitude() <= lightDist + 0.01) { //If there is an object in the way
+    		Hit shadowCheck = world.Closest_Intersection(lightRay);
+    		if ((shadowCheck.dist * shadowRay.direction).magnitude_squared() <= (lightDist + 0.01)) { //If there is an object in the way
     			continue; //Don't add diffuse and specular
     		}
     	}
 
     	//Calculate diffuse 
-    	double angle = max(dot(normal, lightRay.direction), 0);
-    	color += (color_diffuse * world.lights[i].Emitted_Light(lightRay.direction) * angle);
+    	double angle = std::max(dot(normal, lightRay.direction), 0.0);
+      vec3 lightColor = world.lights[i]->Emitted_Light(lightRay.direction) / lightDist;
+    	color += (color_diffuse * lightColor * angle);
 
     	//Calculate specular
     	vec3 reflection = lightRay.direction - (2 * dot(lightRay.direction, normal) * normal); //Calculate reflection vector
-    	angle = max(dot(ray, reflection), 0);
-    	color += (color_specular * world.lights[i].Emitted_Light(lightRay.direction) * pow(angle, specular_power));
+    	angle = std::max(dot(ray.direction, reflection), 0.0);
+    	color += (color_specular * lightColor * pow(angle, specular_power));
     }
     return color;
 }
